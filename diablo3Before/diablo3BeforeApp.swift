94dbba5e209bfc0e68a879642c3ff42a744a01df
jinwoong <baseball0020@gmail.com>
2023-04-04 15:50:34 +0900

    Initial Commit

---
+++ diablo3Before/diablo3BeforeApp.swift
@@ -0,0 +1,17 @@
+//
+//  diablo3BeforeApp.swift
+//  diablo3Before
+//
+//  Created by jinwoong Kim on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct diablo3BeforeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
